7e62cbd3fc40063023fe25a44aafd6853cffca5a
adamhenson <adamhenson1979@gmail.com>
2022-12-25 15:10:00 -0500

    chore: examples

--- src/strategies/index.example.ts
+++ src/strategies/index.example.ts
@@ -2,11 +2,22 @@ import { StrategyCollection } from '../types';
 import bullishEngulfing, {
   configs as bullishEngulfingConfigs,
 } from './public/bullishEngulfing';
+import threeWhiteSoldiers, {
+  configs as threeWhiteSoldiersConfigs,
+} from './public/threeWhiteSoldiers';
+
+// import somePrivateStrategy, {
+//   configs as somePrivateStrategyConfigs,
+// } from './private/somePrivateStrategy';
 
 const strategies: StrategyCollection = {
   bullishEngulfing: {
     strategy: bullishEngulfing,
     configs: bullishEngulfingConfigs,
+  },
+  threeWhiteSoldiers: {
+    strategy: threeWhiteSoldiers,
+    configs: threeWhiteSoldiersConfigs,
   },
 };
 
--- src/symbols/index.example.ts
+++ src/symbols/index.example.ts
@@ -1,8 +1,11 @@
 import sAndP500 from './public/sAndP500';
 
+// import somePrivateList from './private/somePrivateList';
+
 type Symbols = Record<string, string[]>;
 const symbols: Symbols = {
   sAndP500,
+  // somePrivateList,
 };
 
 export default symbols;
